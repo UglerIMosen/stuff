6e8fa18301e4f511b1e62faa89af4c990fa3dd32
Thomas Smitshuysen <thomas@smitshuysen.dk>
2021-07-05 10:00:43 +0200

    included integrate function in filters, and more functions for x-ray absorption

--- common/filters.py
+++ common/filters.py
@@ -66,5 +66,14 @@ def derivative(xy_data,order=1,epsilon = 1e-7):
             x = 0.5*(x[:-1]+x[1:])
         return [x,dydx]
     
-    
-    
+def integrate(xy_data):
+    if len(xy_data) == 2:
+        integrated_sum = [xy_data[1][0]*(xy_data[0][1]-xy_data[0][0])]
+        for value,difference in zip(xy_data[1][1:],np.diff(xy_data[0])):
+            integrated_sum.append(integrated_sum[-1]+value*(difference))
+        return [xy_data[0],integrated_sum]
+    else:
+        integrated_sum = [xy_data[0]]
+        for value in xy_data[1:]:
+            integrated_sum.append(integrated_sum[-1]+value)
+        return integrated_sum
--- scientific/XAS.py
+++ scientific/XAS.py
@@ -13,16 +13,43 @@ import warnings
 
 from stuff.common.filters import derivative
 
-def calibrate_edge(edge,ref_edge):
-    #This function calibrates by the average between the minimum and maximum of the first derivative. This is more alternative than taking the minimum of the second derivative. But in terms of real data, the second derivative can be very very noisy. This solution should be close.
+def find_binding_energy(edge):
     dev_edge = derivative(edge)
     edge_2nd_dev_min = (dev_edge[0][np.where(dev_edge[1] == max(dev_edge[1]))[0][0]]+dev_edge[0][np.where(dev_edge[1] == min(dev_edge[1]))[0][0]])/2
+    return edge_2nd_dev_min
 
-    dev_ref = derivative(ref_edge)
-    ref_2nd_dev_min = (dev_ref[0][np.where(dev_ref[1] == max(dev_ref[1]))[0][0]]+dev_ref[0][np.where(dev_ref[1] == min(dev_ref[1]))[0][0]])/2
+def calibrate_edge(edge,ref_edge):
+    #This function calibrates by the average between the minimum and maximum of the first derivative. This is more alternative than taking the minimum of the second derivative. But in terms of real data, the second derivative can be very very noisy. This solution should be close.
+    #dev_edge = derivative(edge)
+    #edge_2nd_dev_min = (dev_edge[0][np.where(dev_edge[1] == max(dev_edge[1]))[0][0]]+dev_edge[0][np.where(dev_edge[1] == min(dev_edge[1]))[0][0]])/2
+    edge_2nd_dev_min = find_binding_energy(edge)
+    ref_2nd_dev_min = find_binding_energy(ref_edge)
+    #dev_ref = derivative(ref_edge)
+    #ref_2nd_dev_min = (dev_ref[0][np.where(dev_ref[1] == max(dev_ref[1]))[0][0]]+dev_ref[0][np.where(dev_ref[1] == min(dev_ref[1]))[0][0]])/2
     return ref_2nd_dev_min-edge_2nd_dev_min
 
-def XANES_linear_combination_fitting(xy_data,y_references,plot=False,references_names=[]):
+def normalise_fluorescence(edge,pre_edge_energy = -0.05, extended_edge_energy = 0.03):
+    bnd_energy = find_binding_energy(edge)
+    pre_edge = np.where(edge[0] < bnd_energy+pre_edge_energy)
+    extended_edge = np.where(edge[0] > bnd_energy+extended_edge_energy)
+
+    dat_x = edge[0][extended_edge[0][0]:extended_edge[0][-1]]
+    dat_y = edge[1][extended_edge[0][0]:extended_edge[0][-1]]
+    slope = np.polyfit(dat_x,dat_y,1)[0]
+    def normalisation_func(x):
+        if x < bnd_energy:
+            return 0
+        elif x >= bnd_energy:
+            return (x-bnd_energy)*slope
+    normal_intensity = [i-normalisation_func(j) for i,j in zip(edge[1],edge[0])]
+
+    pre_edge_y = normal_intensity[pre_edge[0][0]:pre_edge[0][-1]]
+    extended_edge_y = normal_intensity[extended_edge[0][0]:extended_edge[0][-1]]
+    parameters = [np.mean(pre_edge_y),1/(np.mean(extended_edge_y)-np.mean(pre_edge_y))]    
+    intensity = (np.array(normal_intensity)-parameters[0])*parameters[1]
+    return [edge[0], intensity]
+
+def XANES_linear_combination_fitting(xy_data,y_references,plot=False,references_names=[],amplitude_bnds = (0.9,1.1)):
     #y_reference should share x-axis with xy_data
     def best_fit_error(a):
         fit_curve = 0
@@ -33,7 +60,7 @@ def XANES_linear_combination_fitting(xy_data,y_references,plot=False,references_
     bnds = []
     for reference in y_references:
         bnds.append((0,1))
-    bnds.append((0.85,1.15))
+    bnds.append(amplitude_bnds)
     
     con = ({'type': 'eq', 'fun': lambda a: np.sum(a[0:-1])-1})
     results = shgo(best_fit_error, bounds=bnds, constraints=con, options=({'minimize_every_iter': True}))
