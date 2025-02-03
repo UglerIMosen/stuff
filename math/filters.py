977f24f8d3b68560d77d8fa4207d106e5f1d6fd6
UglerIMosen <thomas@smitshuysen.dk>
2025-02-03 14:19:19 +0100

    s

--- math/filters.py
+++ math/filters.py
@@ -63,35 +63,3 @@ def bin_data(xy_data, xrange = False, resolution=1):
     mask = np.logical_and(xy_data[0]>xrange[0],xy_data[0]<xrange[1])
     binned_y_data = (np.histogram(xy_data[0][mask],bins,weights=xy_data[1][mask])[0] / np.histogram(xy_data[0][mask],bins)[0])
     return np.array([binned_x_data,binned_y_data])
-
-def derivative(xy_data,order=1,epsilon = 1e-7):
-    if order == 0:
-        return xy_data
-    else:
-        x  = np.array(xy_data[0])
-        dx = xy_data[0]
-        dy = xy_data[1]
-        for i in range(0,order):
-            dx_w_zeros = np.diff(dx)
-            dy = np.diff(dy)
-            dx = []
-            for value in dx_w_zeros:
-                if value < epsilon:
-                    dx.append(epsilon)
-                else:
-                    dx.append(value)
-            dydx = dy/dx
-            x = 0.5*(x[:-1]+x[1:])
-        return [x,dydx]
-
-def integrate(xy_data):
-    if len(xy_data) == 2:
-        integrated_sum = [xy_data[1][0]*(xy_data[0][1]-xy_data[0][0])]
-        for value,difference in zip(xy_data[1][1:],np.diff(xy_data[0])):
-            integrated_sum.append(integrated_sum[-1]+value*(difference))
-        return [xy_data[0],integrated_sum]
-    else:
-        integrated_sum = [xy_data[0]]
-        for value in xy_data[1:]:
-            integrated_sum.append(integrated_sum[-1]+value)
-        return integrated_sum
